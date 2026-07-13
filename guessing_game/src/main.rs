f603109e9f0a9b3b60b55dffcd17507d9b9c16de
alkestian <cameronblack51@hotmail.co.uk>
2026-07-13 20:32:05 +0100

    error handling and removing second guess print

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,10 +4,9 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number between 1 and 10!");
 
-    let secret = rand::thread_rng().gen_range(1..=100);
-    println!("Your secret number is {}", secret);
+    let secret = rand::thread_rng().gen_range(1..=10);
 
     loop {
         println!("Please enter your guess:");
@@ -15,12 +14,16 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        println!("You guessed: {}", guess.trim());
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please enter a positive number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret) {
             Ordering::Less => println!("Too low"),
