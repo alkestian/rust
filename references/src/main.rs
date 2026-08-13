5c73bbcfbf8d906b3e50d3e022ac265625b5c3ad
alkestian <CameronBlack51@hotmail.co.uk>
2026-08-13 10:02:40 +0100

    chore: references

---
+++ references/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut s1 = String::from("hello");
+
+    let mut len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+
+    change(&mut s1);
+    
+    len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
