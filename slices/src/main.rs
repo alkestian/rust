2e4ee820862e1f41196d494e704739ee16798f89
alkestian <cameron.black@dailypay.com>
2026-08-26 16:33:49 +0100

    slices start

---
+++ slices/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let s = String::from("hello world");
+
+    let first = &first_word(&s);
+    println!("{}", String::to_string(&first));
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
